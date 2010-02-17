6fc271a417e74327ebcdf75fe701636915ae7ff2
sbressler <sbressler@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-17 11:31:12 +0000

    Removed print_r for  object
    git-svn-id: https://svn.wp-plugins.org/edit-flow/trunk@206999 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- php/usergroups.php
+++ php/usergroups.php
@@ -788,7 +788,6 @@ function ef_usergroups_select_form( $selected = array(), $args = null ) {
 		<ul id="<?php echo $list_id ?>" class="<?php echo $list_class ?>">
 		<?php
 		foreach( $usergroups as $usergroup ) {
-			print_r($usergroup);
 			$checked = (in_array($usergroup->slug, $selected)) ? 'checked="checked"' : '';
 			?>
 			<li>
