03d50b197f9c1ee95ebe451f18d26c19a01f9722
jboydston <jboydston@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-11 20:50:53 +0000

    Calendar
    git-svn-id: https://svn.wp-plugins.org/edit-flow/trunk@239820 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/edit_flow.js
+++ js/edit_flow.js
@@ -1,16 +1,1 @@
-jQuery(document).ready(function () {
-	// THIS IS A HACK! YES, A HACK!
-	// Hides all the empty taxonomy links added to the menu due to a WordPress issue
-	jQuery('.wp-submenu > ul > li').each(function() {
-		var $li = jQuery(this);
-		if($li.text() == '') $li.hide();
-	});
-
-	// This should probably be moved to usergroups.js when that file is included
-	jQuery(".follow_all").click(function() {
-		var checked_status = this.checked;
-		jQuery("input[name=" + this.id + "[]]").each(function() {
-			this.checked = checked_status;
-		});
-	});
-});
+jQuery(document).ready(function () {	// THIS IS A HACK! YES, A HACK!	// Hides all the empty taxonomy links added to the menu due to a WordPress issue	jQuery('.wp-submenu > ul > li').each(function() {		var $li = jQuery(this);		if($li.text() == '') $li.hide();	});});
