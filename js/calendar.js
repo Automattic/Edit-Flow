4bc9bd24f9da7fb9ebec960d35ffa49e7f9747dd
danielbachhuber <danielbachhuber@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-27 23:10:12 +0000

    More calendar UI work
    git-svn-id: https://svn.wp-plugins.org/edit-flow/trunk@257990 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/calendar.js
+++ js/calendar.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function () {
-//    alert(window.location.pathname);
+
 	jQuery(".week-list").sortable({
 		placeholder: 'ui-state-highlight',
 		connectWith: '.connectedSortable',
@@ -9,6 +9,11 @@ jQuery(document).ready(function () {
 
             var post = ui.item[0].id;
             var date = ui.item[0].parentNode.id;
+
+						// Javascript is ghetto and doesn't give month names; this is how we generate it
+						var month_names = new Array("January", "February", "March", 
+						"April", "May", "June", "July", "August", "September", 
+						"October", "November", "December");
                 
             // update the post record
             jQuery.post(window.location.href,
@@ -17,7 +22,11 @@ jQuery(document).ready(function () {
 									date: date
 								},
                 function(data) {
-
+									// Remove the prior message (if it exists) and append a new one
+									jQuery('div#message').remove();
+									date = new Date(date);
+									var message = 'Item date changed to '+ month_names[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear() + '.';
+									jQuery('h2').after('<div id="message" class="updated below-h2"><p>'+message+'</p></div>');
                 }
             );
             
