2dda2f1af789359604921da2da0f0d5134a7c94e
danielbachhuber <danielbachhuber@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-27 22:19:35 +0000

    Updated calendar UI to better match standard WP styles; posts also have bigger draggable elements
    git-svn-id: https://svn.wp-plugins.org/edit-flow/trunk@257973 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/calendar.js
+++ js/calendar.js
@@ -7,28 +7,17 @@ jQuery(document).ready(function () {
 		handle: '.item-handle',
         stop: function(e, ui) {
 
-            var $post = ui.item[0].id;
-            var $date = ui.item[0].parentNode.id;
-            var $img = jQuery("#" + $post + ' > span.item-handle')
-            $img.html($img.html().replace(/drag_handle.jpg/, 'saving.gif'));
-            jQuery("#" + $post + " > span.item-handle > img").css("opacity", "1.0");
-            setTimeout( function() { 
-                $img.html($img.html().replace(/saving.gif/, 'drag_handle.jpg')); 
-                jQuery("#" + $post + " > span.item-handle > img").css("opacity", "0.0");                
-                jQuery("#" + $post + " > span.item-handle > img").hover(function() {
-                    jQuery(this).css("opacity", "1.0");
-                });
-                }, 1000 );
+            var post = ui.item[0].id;
+            var date = ui.item[0].parentNode.id;
                 
             // update the post record
             jQuery.post(window.location.href,
-                { post_id: $post, date: $date },
+                { 
+									post_id: post,
+									date: date
+								},
                 function(data) {
-                    $img.html($img.html().replace(/saving.gif/, 'drag_handle.jpg')); 
-                    jQuery("#" + $post + " > span.item-handle > img").css("opacity", "0.0");     
-                    jQuery("#" + $post + " > span.item-handle > img").hover(function() {
-                        jQuery(this).css("opacity", "1.0");
-                    });
+
                 }
             );
             
