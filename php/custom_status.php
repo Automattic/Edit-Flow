42eea334bd48018cf8f24e4123a22b2bcef5d39a
sbressler <sbressler@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-05 01:26:09 +0000

    PHP5 is a requirement for EF anyway, so go back to using the PHP5 way in custom_status
    git-svn-id: https://svn.wp-plugins.org/edit-flow/trunk@285221 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- php/custom_status.php
+++ php/custom_status.php
@@ -243,9 +243,9 @@ class custom_status {
 	function update_custom_status ( $status_id, $args = array() ) {
 		global $edit_flow;
 		
-		$default_status = $this->ef_get_default_custom_status();
+		$default_status = $this->ef_get_default_custom_status()->slug;
 		$defaults = array(
-				'slug' => $default_status->slug
+				'slug' => $default_status
 		);
 
 		// If new status not indicated, use default status
