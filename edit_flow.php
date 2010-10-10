b98c3f01c2f0adbf96236a678161c24b26687fb1
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-10-10 18:40:30 -0400

    Moved primary settings page to its own class, shifted its location to underneath the heading, and updated the constant to the appropriate location

--- edit_flow.php
+++ edit_flow.php
@@ -44,8 +44,7 @@ include_once('php/editorial_metadata.php');
 define( 'EDIT_FLOW_VERSION' , '0.5.3');
 define( 'EDIT_FLOW_FILE_PATH' , dirname(__FILE__).'/'.basename(__FILE__) );
 define( 'EDIT_FLOW_URL' , plugins_url(plugin_basename(dirname(__FILE__)).'/') );
-define( 'EDIT_FLOW_MAIN_PAGE' , 'admin.php?page=edit-flow/edit_flow' );
-define( 'EDIT_FLOW_SETTINGS_PAGE' , 'admin.php?page=edit-flow/settings' );
+define( 'EDIT_FLOW_SETTINGS_PAGE' , 'admin.php?page=edit-flow/edit_flow' );
 define( 'EDIT_FLOW_CUSTOM_STATUS_PAGE' , 'admin.php?page=edit-flow/custom_status' );
 define( 'EDIT_FLOW_PREFIX' , 'ef_' );
 define( 'EDIT_FLOW_CALENDAR_PAGE', 'index.php?page=edit-flow/calendar');
@@ -292,7 +291,7 @@ class edit_flow {
 		/**
 		 * Add Top-level Admin Menu
 		 */
-		add_menu_page(__('Edit Flow', 'edit-flow'), __('Edit Flow', 'edit-flow'), 'manage_options', $this->get_page('edit-flow'), array(&$this, 'toplevel_page'));
+		add_menu_page(__('Edit Flow', 'edit-flow'), __('Edit Flow', 'edit-flow'), 'manage_options', $this->get_page('edit-flow'), array(&$this->settings, 'settings_page'));
 		
 		// Add sub-menu page for Custom statuses		
 		add_submenu_page( $this->get_page('edit-flow'), __('Custom Status', 'edit-flow'), __('Custom Status', 'edit-flow'), 'manage_options', $this->get_page('custom_status'), array(&$this->custom_status,'admin_page'));
@@ -303,9 +302,6 @@ class edit_flow {
 		
 		// Add sub-menu page for User Groups
 		add_submenu_page($this->get_page('edit-flow'), __('User Groups', 'edit-flow'), __('User Groups', 'edit-flow'), 'manage_options', $this->get_page('usergroups'), array(&$this->usergroups,'admin_page'));
-		
-		// Add sub-menu page for Settings		
-		add_submenu_page($this->get_page('edit-flow'), __('Settings', 'edit-flow'), __('Settings', 'edit-flow'), 'manage_options', $this->get_page('settings'), array(&$this, 'settings_page'));
 		
 		// Add sub-menu page for Calendar
 		if ( $this->calendar_viewable() ) {
@@ -337,215 +333,6 @@ class edit_flow {
 		if( $pagenow == 'index.php' && $plugin_page == $this->get_page( 'calendar' ) )
 			wp_enqueue_script('edit_flow-calendar-js', EDIT_FLOW_URL.'js/calendar.js', array('jquery', 'jquery-ui-core', 'jquery-ui-sortable'), false, true);
 	}
-	
-	/* 
-	 * Adds Settings page for Edit Flow.
-	 */
-	function settings_page( ) {
-		global $wp_roles;
-		
-		$msg = null;
-		if( array_key_exists( 'updated', $_GET ) && $_GET['updated']=='true' ) { $msg = __('Settings Saved', 'edit-flow'); }
-		
-		?>
-			<div class="wrap">
-				<div class="icon32" id="icon-options-general"><br/></div>
-				<h2><?php _e('Edit Flow Settings', 'edit-flow') ?></h2>
-				
-				<?php if($msg) : ?>
-					<div class="updated fade" id="message">
-						<p><strong><?php echo $msg ?></strong></p>
-					</div>
-				<?php endif; ?>
-				
-				<form method="post" action="options.php">
-					<?php settings_fields($this->options_group); ?>
-					
-					<table class="form-table">
-						<tr valign="top">
-							<th scope="row"><strong><?php _e('Custom Statuses', 'edit-flow') ?></strong></th>
-							<td>
-								<p>
-									<label for="custom_statuses_enabled">
-										<input type="checkbox" name="<?php  echo $this->get_plugin_option_fullname('custom_statuses_enabled') ?>" value="1" <?php echo ($this->get_plugin_option('custom_statuses_enabled')) ? 'checked="checked"' : ''; ?> id="custom_statuses_enabled" /> 
-										<?php _e('Enable Custom Statuses for Posts', 'edit-flow') ?>
-									</label> <br />
-									<span class="description"><?php printf( __('Enabling this option allow you to assign custom statuses to your Posts. <br />Custom Statuses can be created and managed here: <a href="%s">Edit Flow > Custom Status</a>', 'edit-flow'), EDIT_FLOW_CUSTOM_STATUS_PAGE) ?></span>
-								</p>
-								<p>
-									<label for="pages_custom_statuses_enabled">
-										<input type="checkbox" name="<?php  echo $this->get_plugin_option_fullname('pages_custom_statuses_enabled') ?>" value="1" <?php echo ($this->get_plugin_option('pages_custom_statuses_enabled')) ? 'checked="checked"' : ''; ?> id="pages_custom_statuses_enabled" /> 
-										<?php _e('Enable Custom Statuses for Pages', 'edit-flow') ?>
-									</label> <br />
-									<span class="description"><?php _e('Enabling this option allow you to assign custom statuses to your Pages.') ?></span>
-								</p>
-								<p>
-									<label for="custom_status_default_status">
-										<?php _e('Default Status for new posts', 'edit-flow') ?>
-									</label>
-									<select name="<?php  echo $this->get_plugin_option_fullname('custom_status_default_status') ?>" id="custom_status_default_status">
-										
-										<?php $statuses = $this->custom_status->get_custom_statuses() ?>
-										<?php foreach($statuses as $status) : ?>
-										
-											<?php $selected = ($this->get_plugin_option('custom_status_default_status')==$status->slug) ? 'selected="selected"' : ''; ?>
-											<option value="<?php esc_attr_e($status->slug) ?>" <?php echo $selected ?>>
-												<?php esc_html_e($status->name); ?>
-											</option>
-											
-										<?php endforeach; ?>
-									</select>
-									<br />
-									<span class="description"><?php _e('The default status that is applied when a new post is created.', 'edit-flow') ?></span>
-								</p>
-								
-								<p>
-									<label for="status_dropdown_visible">
-										<input type="checkbox" name="<?php echo $this->get_plugin_option_fullname('status_dropdown_visible') ?>" value="1" <?php echo ($this->get_plugin_option('status_dropdown_visible')) ? 'checked="checked"' : ''; ?> id="status_dropdown_visible" />
-										<?php _e('Always show status dropdown', 'edit-flow') ?>
-									</label> <br />
-									<span class="description"><?php _e('Enabling this option will keep the "Status" dropdown visible at all times when editing posts and pages to allow for easy updating of statuses.', 'edit-flow') ?></span>
-								</p>
-									
-							</td>
-						</tr>
-						
-						<tr valign="top">
-							<th scope="row"><strong><?php _e('Calendar', 'edit-flow') ?></strong></th>
-							<td>
-								<p>
-									<label for="calendar_enabled">
-										<input type="checkbox" name="<?php  echo $this->get_plugin_option_fullname('calendar_enabled') ?>" value="1" <?php checked($this->get_plugin_option('calendar_enabled')); ?> id="calendar_enabled" />
-										<?php _e('Enable Edit Flow Calendar', 'edit-flow') ?>
-									</label> <br />
-									<span class="description"><?php _e('This enables the Edit Flow Calendar to view editorial content at a glance.', 'edit-flow') ?></span>
-								</p>
-								<?php
-								/* Options for modifying the roles with calendar viewing privileges, though the logic to actually modify those roles was never written. Just didn't want to delete this yet.
-								<p>
-									<strong>Roles that can view calendar</strong><br />
-									<?php foreach($wp_roles->get_names() as $role => $role_name) :
-										if ( $wp_roles->is_role( $role ) ) :
-											$target_role =& get_role( $role );
-											$role_has_cap = $target_role->has_cap( 'view_calendar' );
-											?>
-											<label for="calendar_view_<?php echo $role; ?>">
-												<input type="checkbox" id="calendar_view_<?php echo $role; ?>" value="<?php echo $role; ?>" <?php echo ($role_has_cap ? 'checked="yes"' : '');?> style="margin-bottom: 5px;" />
-												<?php _e($role_name, 'edit-flow') ?>
-											</label>
-											<br />
-										<?php endif; ?>
-									<?php endforeach; ?>
-									<span class="description"><?php _e('Select which roles above may view the Edit Flow Calendar.', 'edit-flow') ?></span>
-								</p>
-								*/
-								?>
-							</td>
-						</tr>
-						
-						<tr valign="top">
-							<th scope="row"><strong><?php _e('Dashboard', 'edit-flow') ?></strong></th>
-							<td>
-								<p>
-									<label for="dashboard_widgets_enabled">
-										<input type="checkbox" name="<?php  echo $this->get_plugin_option_fullname('dashboard_widgets_enabled') ?>" value="1" <?php echo ($this->get_plugin_option('dashboard_widgets_enabled')) ? 'checked="checked"' : ''; ?> id="dashboard_widgets_enabled" />
-										<?php _e('Enable Dashboard Widgets', 'edit-flow') ?>
-									</label> <br />
-									<span class="description"><?php _e('Enables a special set of dashboard widgets for use with Edit Flow. Enable this setting to view the list of available widgets.', 'edit-flow') ?></span>
-								</p>
-								<?php if($this->get_plugin_option('dashboard_widgets_enabled')) : ?>
-								<p>
-									<label for="post_status_widget_enabled">
-										<input type="checkbox" name="<?php  echo $this->get_plugin_option_fullname('post_status_widget_enabled') ?>" value="1" <?php echo ($this->get_plugin_option('post_status_widget_enabled')) ? 'checked="checked"' : ''; ?> id="post_status_widget_enabled" />
-										<?php _e('Enable Post Status Dashboard Widget', 'edit-flow') ?>
-									</label> <br />
-									<span class="description"><?php _e('Gives you an at-a-glance view of the current status of your unpublished content.', 'edit-flow') ?></span>
-								</p>
-								<p>
-									<label for="quickpitch_widget_enabled">
-										<input type="checkbox" name="<?php  echo $this->get_plugin_option_fullname('quickpitch_widget_enabled') ?>" value="1" <?php echo ($this->get_plugin_option('quickpitch_widget_enabled')) ? 'checked="checked"' : ''; ?> id="quickpitch_widget_enabled" />
-										<?php _e('Enable QuickPitch Dashboard Widget') ?>
-									</label> <br />
-									<span class="description"><?php _e('Gives you the ability to create a pitch or draft post from the dashboard.', 'edit-flow') ?></span>
-								</p>
-								<p>
-									<label for="myposts_widget_enabled">
-										<input type="checkbox" name="<?php  echo $this->get_plugin_option_fullname('myposts_widget_enabled') ?>" value="1" <?php echo ($this->get_plugin_option('myposts_widget_enabled')) ? 'checked="checked"' : ''; ?> id="myposts_widget_enabled" />
-										<?php _e('Enable My Posts Dashboard Widget') ?>
-									</label> <br />
-									<span class="description"><?php _e('Gives you quick access to Posts that you are currently following.', 'edit-flow') ?></span>
-								</p>
-								<?php endif; ?>
-								
-							</td>
-						</tr>
-						
-						<tr valign="top">
-							<th scope="row"><strong><?php _e('Notifications', 'edit-flow') ?></strong></th>
-							<td>
-								<p>
-									<label for="notifications_enabled">
-										<input type="checkbox" name="<?php  echo $this->get_plugin_option_fullname('notifications_enabled') ?>" value="1" <?php echo ($this->get_plugin_option('notifications_enabled')) ? 'checked="checked"' : ''; ?> id="notifications_enabled" />
-										<?php _e('Enable Email Notifications', 'edit-flow') ?>
-									</label> <br />
-									<span class="description"><?php _e('This sends out emails notifications whenever certain actions related to posts occur. Currently, only the following notifications are available: a) email notification on post status change; and b) email notification when an editorial comment is added to a post.', 'edit-flow') ?></span>
-								</p>
-								<p>
-									<label for="always_notify_admin">
-										<input type="checkbox" name="<?php  echo $this->get_plugin_option_fullname('always_notify_admin') ?>" value="1" <?php echo ($this->get_plugin_option('always_notify_admin')) ? 'checked="checked"' : ''; ?> id="always_notify_admin" />
-										<?php _e('Always Notify Admin', 'edit-flow') ?>
-									</label> <br />
-									<span class="description"><?php _e('If notifications are enabled, the blog administrator will always receive notifications.', 'edit-flow') ?></span>
-								</p>
-							</td>
-						</tr>
-						
-					</table>
-									
-					<p class="submit">
-						<input type="hidden" name="<?php echo $this->get_plugin_option_fullname('version') ?>" value="<?php echo ($this->get_plugin_option('version')) ?>" />
-					    <input type="hidden" name="<?php echo $this->get_plugin_option_fullname('custom_status_filter') ?>" value="<?php echo ($this->get_plugin_option('custom_status_filter')) ?>" id="custom_status_filter" />
-				        <input type="hidden" name="<?php echo $this->get_plugin_option_fullname('custom_category_filter') ?>" value="<?php echo ($this->get_plugin_option('custom_category_filter')) ?>" id="custom_category_filter" />
-				        <input type="hidden" name="<?php echo $this->get_plugin_option_fullname('custom_author_filter') ?>" value="<?php echo ($this->get_plugin_option('custom_author_filter')) ?>" id="custom_author_filter" />
-						<input type="submit" class="button-primary" value="<?php _e('Save Changes', 'edit-flow') ?>" />
-					</p>
-				</form>
-			</div>
-		<?php 
-	}
-	
-	/**
-	 * Add Top-level Admin Menu
-	 * This should be exported into a separate file (?)
-	 */
-	function toplevel_page() {
-		?>
-		<div class="wrap">
-			<div id="icon-edit" class="icon32"><br /></div>
-			<h2>Edit Flow</h2>
-			
-			<p>The overall goal of this plugin is to improve WordPress' admin for a multi-user newsroom's editorial workflow.</p>
-				
-			<p>Through discussions with existing newsrooms that use WordPress as their CMS of choice, we have identified the following weak points within the WordPress Administration interface in the context of a multi-user environment:</p>
-				
-			<ul>
-				<li>- The editorial workflow is limited and does not scale well where numerous individuals interact with a single Post or where more complex editing workflows are required;
-				</li>
-				<li>- The editorial workflow is not conducive to planning of future content (while the Draft feature does facilitate this to some extent, the existing feature set does not scale in scenarios where different users are responsible for different components of a post, or different steps within the workflow.
-				</li>
-				<li>- The ability for users to communicate within the Administration interface is limited, both the planning of future Posts/assignments and during the editing process.
-				</li>
-			</ul>
-			
-			<p>To help you use WordPress more effectively, we're adding cool features that make you go "Yay!" and add all kinds of efficiency to your publishing workflow!</p>
-			
-			<p>Learn more about <a href="http://www.editflow.org" target="_blank">Edit Flow</a>. Got an itch that Edit Flow doesn't scratch yet? <a href="http://mailto:editflow@copress.org">Get in touch!</a></p>
-			
-			<p><small>One day we'll put something a little more useful here. Like an ice-cream maker. Or a self-destruct button.</small></p>
-			
-		</div>
-		<?php 
-	} // END: toplevel_menu()
 
     function calendar() {
 		include('php/templates/calendar.php');
