e0e6ba30d6d14dbe5e7a00440a54fa9cceb17800
Mike Bijon <mikebijon@gmail.com>
2013-04-27 11:57:27 -0700

    Travis PHP <5.4 doesn't allow SSL/https :: use non-SSL Github

--- tests/test_wordpress_plugin_tests.php
+++ tests/test_wordpress_plugin_tests.php
@@ -28,7 +28,7 @@ class WP_Test_WordPress_Plugin_Tests extends WP_UnitTestCase {
 		//trunk is always "master" in github terms, but WordPress has a specific way of describing it
 		//grab the exact version number to verify that we're on trunk
 		if ( $requested_version == 'master' ) {
-			$file = file_get_contents( 'https://raw.github.com/WordPress/WordPress/master/wp-includes/version.php' );
+			$file = file_get_contents( 'http://raw.github.com/WordPress/WordPress/master/wp-includes/version.php' );
 			preg_match( '#\$wp_version = \'([^\']+)\';#', $file, $matches );
 			$requested_version = $matches[1];
 		}
