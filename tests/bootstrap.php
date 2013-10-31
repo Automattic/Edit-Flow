e9969b91b446bfe7d5909cbb49fcbe83456ee5bf
Will Van Wazer <will@willvanwazer.com>
2013-10-31 00:19:05 -0400

    Another travis test.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
 require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../Edit-Flow.php';
+	require dirname( __FILE__ ) . '/../edit_flow.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
