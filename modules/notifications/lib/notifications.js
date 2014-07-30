1820d66e530ea8d2f2c720e6fa5c19c2ececa2c6
Ryan Domingue <ryan@ryandomingue.com>
2014-07-29 22:56:16 -0700

    Display the follower count message in the post submit box

--- modules/notifications/lib/notifications.js
+++ modules/notifications/lib/notifications.js
@@ -6,6 +6,9 @@ jQuery(document).ready(function($) {
 		post_id: $('#post_ID').val(),
 	};
 
+	// Display the user/group follower count in the post submit box
+	ef_displayFollowerCountInSubmitBox();
+
 	$('.ef-post_following_list li input:checkbox, .ef-following_usergroups li input:checkbox').click(function() {
 		var user_group_ids = [];
 		var parent_this = $(this);
@@ -28,6 +31,8 @@ jQuery(document).ready(function($) {
 			url : (ajaxurl) ? ajaxurl : wpListL10n.url,
 			data : params,
 			success : function(x) { 
+				// Update the user/group follower count in the post submit box
+				ef_displayFollowerCountInSubmitBox();
 				var backgroundColor = parent_this.css( 'background-color' );
 				$(parent_this.parent().parent())
 					.animate( { 'backgroundColor':'#CCEEBB' }, 200 )
@@ -39,3 +44,44 @@ jQuery(document).ready(function($) {
 		});
 	});
 });
+
+/**
+ * Count the users and user groups who will be notified of a status change
+ * Display the message in the submit box
+ */
+var ef_displayFollowerCountInSubmitBox = function() {
+	var checkedFollowers, checkedUserGroups, countDisplay, message = '', followerMessage = '', userGroupMessage = '',conjunction = '';
+
+	// Get checked checkboxes
+	checkedFollowers = jQuery('.ef-post_following_list li input:checkbox:checked');
+	checkedUserGroups = jQuery('#ef-following_usergroups li input:checkbox:checked');
+	// checkedFollowers includes user groups
+	userCount = checkedFollowers.length - checkedUserGroups.length;
+	userGroupCount = checkedUserGroups.length;
+
+	// The <span> within which the message will be displayed
+	countDisplay = jQuery('#post-follower-count-display');
+
+	// Create the individual messages
+	if (userCount > 0) {
+		followerMessage = userCount + ((userCount === 1) ? ' user' : ' users');
+	}
+	if (userGroupCount > 0) {
+		userGroupMessage = userGroupCount + ((userGroupCount === 1) ? ' user group' : ' user groups');
+	}
+
+	if (userCount > 0 && userGroupCount > 0) {
+		// Both will be displayed, so we need a conjunction
+		conjunction = ' & ';
+		message = (followerMessage + conjunction + userGroupMessage);
+	} else if (userCount > 0 || userGroupCount > 0) {
+		// Only one will be displayed, the other is an empty string
+		message = (followerMessage + userGroupMessage);
+	} else {
+		// Default message
+		message = 'none';
+	}
+
+	// Print the message
+	countDisplay.html(message);
+};
