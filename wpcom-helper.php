96bdd916a8a1be6453cefc018a7339a9625db632
Philip John <phil@philipjohn.co.uk>
2017-01-24 17:55:18 +0000

    Move wpcom-helper.php into version control. Git-first development ftw.

---
+++ wpcom-helper.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Ensure Edit Flow is instantiated
+ */
+add_action( 'after_setup_theme', 'EditFlow' );
+
+/**
+ * Don't load caps on install for WP.com. Instead, let's add
+ * them with the WP.com + core caps approach
+ */
+add_filter( 'ef_kill_add_caps_to_role', '__return_true' );
+add_filter( 'ef_view_calendar_cap', function() { return 'edit_posts'; } );
+add_filter( 'ef_view_story_budget_cap', function() { return 'edit_posts'; } );
+add_filter( 'ef_edit_post_subscriptions_cap', function() { return 'edit_others_posts'; } );
+add_filter( 'ef_manage_usergroups_cap', function() { return 'manage_options'; } );
+
+/**
+ * Edit Flow loads modules after plugins_loaded, which has already been fired on WP.com
+ * Let's run the method at after_setup_themes
+ */
+add_filter( 'after_setup_theme', 'edit_flow_wpcom_load_modules' );
+function edit_flow_wpcom_load_modules() {
+	global $edit_flow;
+	if ( method_exists( $edit_flow, 'action_ef_loaded_load_modules' ) )
+		$edit_flow->action_ef_loaded_load_modules();
+}
+
+/**
+ * Share A Draft on WordPress.com breaks when redirect canonical is enabled
+ * get_permalink() doesn't respect custom statuses
+ *
+ * @see http://core.trac.wordpress.org/browser/tags/3.4.2/wp-includes/canonical.php#L113
+ */
+add_filter( 'redirect_canonical', 'edit_flow_wpcom_redirect_canonical' );
+function edit_flow_wpcom_redirect_canonical( $redirect ) {
+
+	if ( ! empty( $_GET['shareadraft'] ) )
+		return false;
+
+	return $redirect;
+}
