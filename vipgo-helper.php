52d63e26a9afb6fd7e23ffb37ab1b443311c06f0
root <root@d6f09b489363>
2017-01-24 15:59:48 +0000

    Adding vipgo-helper.php in order to allow developers to load the plugin using the `wpcom_vip_load_plugin()` function
    Since loading the plugin from within a theme's functions.php is happenning after the `plugins_loaded` hook is triggerred, we need to initiate the plugin on the `after_setup_theme` hook.
    
    Since the plugin's default directory name does not match the .php file, when using the `wpcom_vip_load_plugin()` function following notication is necessary:
    
    ```
    wpcom_vip_load_plugin( 'Edit-Flow/edit_flow.php' );
    ```
    
    Fixes #368

---
+++ vipgo-helper.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Ensure Edit Flow is instantiated
+ */
+add_action( 'after_setup_theme', 'EditFlow' );
+
+/**
+ * Edit Flow loads modules after plugins_loaded, which has already been fired when loading via wpcom_vip_load_plugins
+ * Let's run the method at after_setup_themes
+ */
+add_filter( 'after_setup_theme', 'edit_flow_wpcom_load_modules' );
+function edit_flow_wpcom_load_modules() {
+	global $edit_flow;
+	if ( method_exists( $edit_flow, 'action_ef_loaded_load_modules' ) )
+		$edit_flow->action_ef_loaded_load_modules();
+}
