6c3aa638cfdc18b83a9904c0cd4ee4110c199544
raduconst <radu.constantin@pixelgrade.com>
2018-01-25 19:05:45 +0200

    Using alias for PHP7

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,6 +9,9 @@ function _manually_load_plugin() {
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
+if (!class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase'))
+	class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+
 require $_tests_dir . '/includes/bootstrap.php';
 
 require dirname( __FILE__ ) . '/testcase-edit-flow-ajax.php';
