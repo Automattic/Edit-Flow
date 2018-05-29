363d272a532436d775d59b98fe3c7ea6d067a587
Stefano Ottolenghi <stejey@gmail.com>
2018-05-29 22:03:51 +0200

    Removed whitespaces introduced in last commit

--- modules/dashboard/widgets/dashboard-notepad.php
+++ modules/dashboard/widgets/dashboard-notepad.php
@@ -78,7 +78,7 @@ class EF_Dashboard_Notepad_Widget {
 		$current_note = ( ! empty( $posts[0]->post_content ) ) ? $posts[0]->post_content : '';
 		$current_id = ( ! empty( $posts[0]->ID ) ) ? $posts[0]->ID : 0;
 		$current_post = ( ! empty( $posts[0] ) ) ? $posts[0] : false;
-		
+
 		if ( $current_post )
 			$last_updated = '<span id="dashboard-notepad-last-updated">' . sprintf( __( '%1$s last updated on %2$s', 'edit-flow' ), get_user_by( 'id', $current_post->post_author )->display_name, get_the_time( get_option( 'date_format' ) . ' ' . get_option( 'time_format' ), $current_post ) ) . '</span>';
 		else
