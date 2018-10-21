cf3d59437ad4497de8ba156440f9ac1dfcf94997
Indra Lukmana <indralukmana@live.com>
2018-10-21 23:08:46 +0700

    turn off unused javascript

--- modules/user-groups/lib/user-groups.js
+++ modules/user-groups/lib/user-groups.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function () {
-	jQuery('ul#ef-post_following_users li').quicksearch({
-		position: 'before',
-		attached: 'ul#ef-post_following_users',
-		loaderText: '',
-		delay: 100
-	})
-	jQuery('#ef-usergroup-users ul').listFilterizer();
+	// jQuery('ul#ef-post_following_users li').quicksearch({
+	// 	position: 'before',
+	// 	attached: 'ul#ef-post_following_users',
+	// 	loaderText: '',
+	// 	delay: 100
+	// })
+	// jQuery('#ef-usergroup-users ul').listFilterizer();
 });
