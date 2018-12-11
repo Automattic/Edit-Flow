c68815c23b8ae8f1db3b1c08138943b89c685ff5
Mohammad Jangda <batmoo@gmail.com>
2018-12-11 11:19:10 -0500

    Apply suggestions from code review
    Version the assets when calling `wp_enqueue_scripts`. Props @mjangda
    
    Co-Authored-By: rinatkhaziev <rinat.khaziev@gmail.com>

--- modules/custom-status/compat/block-editor.php
+++ modules/custom-status/compat/block-editor.php
@@ -13,8 +13,8 @@ class EF_Custom_Status_Block_Editor_Compat {
 		if ( $this->ef_module->disable_custom_statuses_for_post_type() || !function_exists( 'is_gutenberg_page' ) || ! is_gutenberg_page() )
 			return;
 
-		wp_enqueue_style( 'edit-flow-block-custom-status', EDIT_FLOW_URL . 'blocks/dist/custom-status.editor.build.css', false, null );
-		wp_enqueue_script( 'edit-flow-block-custom-status', EDIT_FLOW_URL . 'blocks/dist/custom-status.build.js', array( 'wp-blocks', 'wp-element', 'wp-edit-post', 'wp-plugins', 'wp-components' ) );
+		wp_enqueue_style( 'edit-flow-block-custom-status', EDIT_FLOW_URL . 'blocks/dist/custom-status.editor.build.css', false, EDIT_FLOW_VERSION );
+		wp_enqueue_script( 'edit-flow-block-custom-status', EDIT_FLOW_URL . 'blocks/dist/custom-status.build.js', array( 'wp-blocks', 'wp-element', 'wp-edit-post', 'wp-plugins', 'wp-components' ), EDIT_FLOW_VERSION );
 
 		wp_localize_script( 'edit-flow-block-custom-status', 'EditFlowCustomStatuses', $this->get_custom_statuses() );
 	}
