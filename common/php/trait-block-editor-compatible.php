403eb970cdab032effe0ec7504eb7abfdc1e8173
Rinat Khaziev <rinat.khaziev@gmail.com>
2018-12-13 18:20:38 -0500

    Better docs for Block_Editor_Compatible trait, re-work the logic that determines whether compat classes should be loaded

--- common/php/trait-block-editor-compatible.php
+++ common/php/trait-block-editor-compatible.php
@@ -1,46 +1,130 @@
 <?php
 /**
- * Experiment: a trait that abstracts the logic to overwrite anything that a specific module does.
- * A prime use case would be not enqueueing EF classic editor assets.
- *
+ * Block_Editor_Compatible aims to be an abstract enough compatibility logic
  */
+
+ // phpcs:disable WordPressVIPMinimum.Variables.VariableAnalysis.SelfOutsideClass
+
 trait Block_Editor_Compatible {
-  protected $ef_module;
-  protected $hooks;
+	/**
+	 * Holds the reference to the Module's object
+	 *
+	 * @var EF_Module
+	 */
+	protected $ef_module;
+	/**
+	 * Holds associative array of hooks and their respective callbacks
+	 *
+	 * @var array
+	 */
+	protected $hooks = [];
+	protected static $active_plugins;
 
-  /**
-   * This method handles init of Module Compat
-   *
-   * @param EF_Module $module_instance
-   * @param array $hooks associative array of hooks and their respective callbacks
-   *
-   * @todo handle priorities too.
-   *
-   * @return void
-   */
-  function __construct( $module_instance, $hooks = [] ) {
-    $this->ef_module = $module_instance;
-    $this->hooks = $hooks;
-    // Highly debatable
-    add_action( 'admin_init', [ $this, 'action_admin_init' ], 0 );
-  }
+	/**
+	 * This method handles init of Module Compat
+	 *
+	 * @param EF_Module $module_instance
+	 * @param array $hooks associative array of hooks and their respective callbacks
+	 *
+	 *
+	 * @return void
+	 */
+	function __construct( $module_instance, $hooks = [] ) {
+		$this->ef_module = $module_instance;
+		$this->hooks = $hooks;
 
-  /**
-   * Unhook the module's hooks and use the module's Compat hooks instead.
-   *
-   * This is currently run on admin_init, but needs more testing.
-   *
-   * @since 0.9
-   *
-   * @return void
-   */
-  function action_admin_init() {
-    // This conditional is probably not catching all the instances where Gutenberg might be enabled.
-    if ( ! isset( $_GET['classic-editor'] ) ) {
-      foreach( $this->hooks as $hook => $callback ) {
-        remove_action( $hook, array( $this->ef_module, $callback ) );
-        add_action( $hook, array( $this, $callback ) );
-      }
-    }
-  }
+		if ( is_admin() ) {
+
+			add_action( 'init', [ $this, 'action_init_for_admin' ], 15 );
+		}
+	}
+
+	/**
+	 * Unhook the module's hooks and use the module's Compat hooks instead.
+	 *
+	 * This is currently run on init action, but only when `is_admin` is true.
+	 *
+	 * @since 0.9
+	 *
+	 * @return void
+	 */
+	function action_init_for_admin() {
+		$this->check_active_plugins();
+
+		if ( $this->should_apply_compat() ) {
+			foreach ( $this->hooks as $hook => $callback ) {
+				if ( is_callable( [ $this, $callback ] ) ) {
+					remove_action( $hook, array( $this->ef_module, $callback ) );
+					add_action( $hook, array( $this, $callback ) );
+				}
+			}
+		}
+	}
+
+	function check_active_plugins() {
+		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+		self::$active_plugins = [
+			'classic-editor' => is_plugin_active( 'classic-editor' ),
+			'gutenberg'      => is_plugin_active( 'gutenberg' ),
+		];
+	}
+
+	/**
+	 * Helper function to determine whether we're running WP 5.0.
+	 *
+	 * @return boolean
+	 */
+	public static function is_at_least_50() {
+		return version_compare( get_bloginfo( 'version' ), '5.0', '>=' );
+	}
+
+	/**
+	 * Helper to determine whether either Gutenberg plugin or Classic  Editor plugin is loaded.
+	 *
+	 * @param string $slug
+	 * @return boolean
+	 */
+	public static function is_plugin_active( $slug = '' ) {
+		return isset( self::$active_plugins[ $slug ] ) && self::$active_plugins[ $slug ];
+	}
+
+	/**
+	 * Detect whether we should load compatability for the module.
+	 * This runs very early during request lifecycle and may not be precise. It's better to use `get_current_screen()->is_block_editor()` if it's available.
+	 *
+	 * However, Block Editor can be enabled/disabled on a granular basis via the filters for the core and the plugin versions.
+	 *
+	 * use_block_editor_for_post
+	 * use_block_editor_for_post_type
+	 * gutenberg_can_edit_post_type
+	 * gutenberg_can_edit_post
+	 *
+	 *
+	 * This needs to be handled in the compat hook callback.
+	 *
+	 * If any of $conditions evaluates to TRUE, we should apply compat hooks.
+	 *
+	 * @return boolean
+	 */
+	protected function should_apply_compat() {
+		$conditions = [
+			/**
+			 * 5.0:
+			 *
+			 * Classic editor either disabled or enabled (either via an option or with GET argument).
+			 * It's a hairy conditional :(
+			 */
+			// phpcs:ignore WordPress.VIP.SuperGlobalInputUsage.AccessDetected, WordPress.Security.NonceVerification.NoNonceVerification
+			self::is_at_least_50() && ! self::is_plugin_active( 'classic-editor' ),
+			self::is_at_least_50() && self::is_plugin_active( 'classic-editor' ) && ( get_option( 'classic-editor-replace' ) === 'block' && ! isset( $_GET[ 'classic-editor__forget' ] ) ),
+			self::is_at_least_50() && self::is_plugin_active( 'classic-editor' ) && ( get_option( 'classic-editor-replace' ) === 'classic' && isset( $_GET[ 'classic-editor__forget' ] ) ),
+			/**
+			 * < 5.0 but Gutenberg plugin is active.
+			 */
+			! self::is_at_least_50() && self::is_plugin_active( 'gutenberg' ),
+		];
+
+		return count( array_filter( $conditions, function( $c ) { return (bool) $c; } ) ) > 0;
+	}
 }
--- modules/custom-status/compat/block-editor.php
+++ modules/custom-status/compat/block-editor.php
@@ -1,7 +1,7 @@
 <?php
 class EF_Custom_Status_Block_Editor_Compat {
 	protected $ef_module;
-	// @see commmon/php/trait-block-editor-compatible
+	// @see commmon/php/trait-block-editor-compatible.php
 	use Block_Editor_Compatible;
 
 	/**
@@ -10,8 +10,16 @@ class EF_Custom_Status_Block_Editor_Compat {
 	 * @return void
 	 */
 	function action_admin_enqueue_scripts() {
-		if ( $this->ef_module->disable_custom_statuses_for_post_type() || !function_exists( 'is_gutenberg_page' ) || ! is_gutenberg_page() )
+		/**
+		 * WP_Screen::is_block_editor only available in 5.0. If it's available and is false it's safe to say we should only pass through to the module.
+		 */
+		if ( Block_Editor_Compatible::is_at_least_50() && ! get_current_screen()->is_block_editor() ) {
+			return $this->ef_module->action_admin_enqueue_scripts();
+		}
+
+		if ( $this->ef_module->disable_custom_statuses_for_post_type() ) {
 			return;
+		}
 
 		wp_enqueue_style( 'edit-flow-block-custom-status', EDIT_FLOW_URL . 'blocks/dist/custom-status.editor.build.css', false, EDIT_FLOW_VERSION );
 		wp_enqueue_script( 'edit-flow-block-custom-status', EDIT_FLOW_URL . 'blocks/dist/custom-status.build.js', array( 'wp-blocks', 'wp-element', 'wp-edit-post', 'wp-plugins', 'wp-components' ), EDIT_FLOW_VERSION );
@@ -20,7 +28,7 @@ class EF_Custom_Status_Block_Editor_Compat {
 	}
 
 	/**
-	 * Just a wrapper to make sure we're have simple array instead of associative.
+	 * Just a wrapper to make sure we're have simple array instead of associative one.
 	 *
 	 * @return array Custom statuses.
 	 */
