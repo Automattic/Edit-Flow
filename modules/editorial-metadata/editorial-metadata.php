28333292437238ad6b14dcbf161a8aa221a43507
mikeyarce <mikeyarce@gmail.com>
2021-06-10 15:42:52 -0700

    Fix spacing around double arrow

--- modules/editorial-metadata/editorial-metadata.php
+++ modules/editorial-metadata/editorial-metadata.php
@@ -1600,7 +1600,7 @@ class EF_Editorial_Metadata_List_Table extends WP_List_Table {
 		parent::__construct( array(
 			'plural' => 'editorial metadata',
 			'singular' => 'editorial metadata',
-			'screen'   =>   'ef-editorial-metadata-settings'
+			'screen' => 'ef-editorial-metadata-settings'
 		) );
 	}
 
