eef790e7a0fc342d376328ae7c914945526c2ec1
Dat Hoang <htdat@users.noreply.github.com>
2021-06-14 16:26:09 +0700

    Remove common/js/jquery.quicksearch.js and its mention in user-groups.js

--- modules/user-groups/lib/user-groups.js
+++ modules/user-groups/lib/user-groups.js
@@ -1,9 +1,3 @@
 jQuery(document).ready(function () {
-	jQuery('ul#ef-post_following_users li').quicksearch({
-		position: 'before',
-		attached: 'ul#ef-post_following_users',
-		loaderText: '',
-		delay: 100
-	})
 	jQuery('#ef-usergroup-users ul').listFilterizer();
 });
