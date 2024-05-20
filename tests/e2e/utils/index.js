ea5fe8a92c60dfd682bd02cfa2d84748b89ff501
Alec Geatches <alec.geatches@automattic.com>
2024-05-20 16:17:08 -0600

    Directly interact with future publish date inputs instead of changing values directly

--- tests/e2e/utils/index.js
+++ tests/e2e/utils/index.js
@@ -67,24 +67,15 @@ const schedulePost = async() => {
         .toLocaleDateString( 'en-US' )
         .split( '/' );
 
-    // Set the future date in the post editing screen
-    await page.$eval(
-        '.components-datetime__time-field-day input',
-        ( el, day ) => el.value = day,
-        day
-    );
+    const dayInput = await page.$('.components-datetime__time-field-day input');
+    await dayInput.click({ clickCount: 3 });
+    await dayInput.type( day );
 
-    await page.$eval(
-        '.components-datetime__time-field-month select',
-        ( el, month ) => el.value = month.length === 1 ? '0' + month : month,
-        month
-    );
+    await page.select('.components-datetime__time-field-month select', month.length === 1 ? '0' + month : month );
 
-    await page.$eval(
-        '.components-datetime__time-field-year input',
-        ( el, year ) => el.value = year,
-        year
-    );
+    const yearInput = await page.$('.components-datetime__time-field-year input');
+    await yearInput.click({ clickCount: 3 });
+    await yearInput.type( year );
 
     await publishPost();
 
