9720ed8dea8a28e7db355bc31d79f615bf007f14
Alec Geatches <alec.geatches@automattic.com>
2024-05-22 14:05:48 -0600

    Fix capitalization for plugin file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -29,7 +29,7 @@ require_once "{$_tests_dir}/includes/functions.php";
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/Edit-Flow.php';
+	require dirname( __DIR__ ) . '/edit-flow.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
