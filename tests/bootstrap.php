9df5d58a6955d90e00a40cfc956e378f83f4177e
Alec Geatches <alec.geatches@automattic.com>
2024-05-22 13:57:00 -0600

    Update test files, bootstrap file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,17 +1,12 @@
-
 <?php
 /**
- * PHPUnit bootstrap file
+ * PHPUnit bootstrap file.
  *
  * @package Edit_Flow
  */
-// If we're running in WP's build directory, ensure that WP knows that, too.
-if ( 'build' === getenv( 'LOCAL_DIR' ) ) {
-	define( 'WP_RUN_CORE_TESTS', true );
-}
 
-// Determine the tests directory (from a WP dev checkout).
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
+
 if ( ! $_tests_dir ) {
 	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
@@ -22,7 +17,6 @@ if ( false !== $_phpunit_polyfills_path ) {
 	define( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH', $_phpunit_polyfills_path );
 }
 
-// Give access to tests_add_filter() function.
 if ( ! file_exists( "{$_tests_dir}/includes/functions.php" ) ) {
 	echo "Could not find {$_tests_dir}/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	exit( 1 );
@@ -35,13 +29,15 @@ require_once "{$_tests_dir}/includes/functions.php";
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( __DIR__ ) . '/edit_flow.php';
+	require dirname( dirname( __FILE__ ) ) . '/Edit-Flow.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-// Start up the WP testing environment.
-require $_tests_dir . '/includes/bootstrap.php';
+// Allow wp_mail() in tests from a valid domain name
+tests_add_filter( 'wp_mail_from', function() {
+	return 'admin@localhost.test';
+});
 
-// Load the abstract class for testing AJAX calls of the plugin.
-require dirname( __FILE__ ) . '/testcase-edit-flow-ajax.php';
+// Start up the WP testing environment.
+require "{$_tests_dir}/includes/bootstrap.php";
