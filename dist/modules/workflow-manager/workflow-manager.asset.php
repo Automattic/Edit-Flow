873097afe6997028da566dabebeb3896846114d2
Alec Geatches <alec.geatches@automattic.com>
2024-06-26 14:06:09 -0600

    Commit built fies

--- dist/modules/workflow-manager/workflow-manager.asset.php
+++ dist/modules/workflow-manager/workflow-manager.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-dom-ready', 'wp-element'), 'version' => 'f6d88f9903247a65edb9');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-dom-ready', 'wp-element'), 'version' => '3f1822595c2f6d6e2ea8');
